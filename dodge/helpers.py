cf906bf584868aa49c0235008c2b8ddac98627db
Minjune <minjune8506@gmail.com>
2022-05-18 17:17:52 +0900

    Refact : Dodge Game 구조 변경
    - Class를 이용하여 구조 변경
    - 불필요한 Logic 삭제

---
+++ dodge/helpers.py
@@ -0,0 +1,20 @@
+# Defining the width and height of the screen
+WIDTH = 800  # Global variables
+HEIGHT = 600
+
+PLAYER_SIZE = 15
+
+# Defining the color for player and enemy
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255,255,0)
+GREEN = (0, 255, 0)
+BACKGROUND_COLOR = (0, 0, 0)  # Setting the background color, Black
+
+# Defining an enemy
+ENEMY_SIZE = 10
+ENEMY_LIST = []  # Defining an enemy list to contain multiple enemies
+S = 1
+N = 2
+W = 3
+E = 4
