136a6960ea3a065ecf37296804caba0bb7c89cd2
jaeyeon kim <jakind@naver.com>
2022-05-18 15:48:52 +0900

    센서 추가

--- dodge/dodge.py
+++ dodge/dodge.py
@@ -1,5 +1,6 @@
 # Referred video https://www.youtube.com/watch?v=-8n91btt5d8
 
+from audioop import mul
 import pygame
 import sys
 import random  # Pythons random library
@@ -28,16 +29,16 @@ PLAYER_POS = [WIDTH / 2 - PLAYER_SIZE / 2, HEIGHT / 2 - PLAYER_SIZE / 2]  # Its 
 ENEMY_SIZE = 10
 ENEMY_LIST = []  # Defining an enemy list to contain multiple enemies
 ENEMY_COUNT = 10
+MATRIX = [[False for i in range(WIDTH + 1)] for j in range(HEIGHT + 1)] # Boolean 형태의 map 생성
+
+dx = [0, 1, 1, 1, 0, -1, -1, -1]
+dy = [-1, -1, 0, 1, 1, 1, 0, -1] # 8 방향
 
 # Creating a screen
 screen = pygame.display.set_mode((WIDTH, HEIGHT))  # We have a screen of WIDTH 800 and HEIGHT 600
-
 game_over = False
-
 score = 0  # Initializing the score
-
 clock = pygame.time.Clock()  # It defines a clock
-
 myFont = pygame.font.SysFont("monospace", 35)  # Defining the font in pygame (Monospace is font and 35 is in pixels)
 endFont = pygame.font.SysFont("comicsansms", 40, True, False)
 
@@ -61,13 +62,14 @@ class Enemy:
 
     def drop_enemies(ENEMY_LIST):
         randomValue = random.randint(1, 4)  # It generates a random value betwee 0 and 4
-        if len(ENEMY_LIST) < ENEMY_COUNT:  # When the no. of elements inside the list is less than 10
+        if len(ENEMY_LIST) < 20:  # When the no. of elements inside the list is less than 10
             if randomValue == 1:
                 #남쪽
                 x_pos = random.randint(0, WIDTH)  # Assigning the x-coordinate to the new enemy randomly.
                 y_pos = HEIGHT
                 y_speed = -1
                 x_speed = random.randint(-1, 1)
+                MATRIX[y_pos][x_pos] = True;
                 ENEMY_LIST.append([x_pos, y_pos, x_speed, y_speed])  # It appends new enemy coordinates to the enemy list
             elif randomValue == 2:
                 #북쪽
@@ -75,6 +77,7 @@ class Enemy:
                 y_pos = 0
                 y_speed = 1
                 x_speed = random.randint(-1, 1)
+                MATRIX[y_pos][x_pos] = True;
                 ENEMY_LIST.append([x_pos, y_pos, x_speed, y_speed])  # It appends new enemy coordinates to the enemy list
             elif randomValue == 3:
                 #서쪽
@@ -82,6 +85,7 @@ class Enemy:
                 y_pos = random.randint(0, HEIGHT)
                 y_speed = random.randint(-1, 1)
                 x_speed = 1
+                MATRIX[y_pos][x_pos] = True;
                 ENEMY_LIST.append([x_pos, y_pos, x_speed, y_speed])  # It appends new enemy coordinates to the enemy list
             elif randomValue == 4:
                 #동쪽
@@ -89,28 +93,73 @@ class Enemy:
                 y_pos = random.randint(0, HEIGHT)
                 y_speed = random.randint(-1, 1)
                 x_speed = -1
+                MATRIX[y_pos][x_pos] = True; # 방문처리 추가
                 ENEMY_LIST.append([x_pos, y_pos, x_speed, y_speed])  # It appends new enemy coordinates to the enemy list
         
+    def get_input() : # 센서를 통해서 장애물 감지
+        inputs = [1.0] * 4 # 4 개 짜리 배열 (상, 하, 좌, 우)
+        y = int(PLAYER_POS[1])
+        x = int(PLAYER_POS[0])
+        
+        detectRange = 200
+        mulNumber = 1 / detectRange
+
+        for i in range((y - detectRange), (y + (detectRange + 1))) : # 주변 위 아래 오른쪽 왼쪽 5칸씩 체크
+            for j in range((x - detectRange), (x + (detectRange + 1))) :
+                if i == y and x == j :
+                    continue
+
+                if i < 0 or i > HEIGHT :
+                    break                    
+
+                if j < 0 or j > WIDTH : # 범위를 나간 경우
+                    continue # 해당 방향으로는 움직일 수 없음        
+
+                if MATRIX[i][j] : # 장애물이 있는 경우                
+                    ny = y - i
+                    nx = x - j               
+
+                    if ny > 0: # ny 가 양수인 경우 (위)
+                        inputs[0] = min(inputs[0], (mulNumber * abs(ny)))
+                    elif ny < 0 : # ny 가 음수인 경우 (아래)
+                        inputs[1] = min(inputs[1], (mulNumber * abs(ny)))
+
+                    if nx > 0 : # nx 가 양수인 경우 (왼쪽)
+                        inputs[2] = min(inputs[2], (mulNumber * abs(nx)))
+                    elif nx < 0 : # nx 가 음수인 경우 (오른쪽)
+                        inputs[3] = min(inputs[3], (mulNumber * abs(nx)))
+        
+        print(inputs) # 출력
+        return inputs # 장애물이 해당 방향에 없는 경우는 1, 아니면 센서값
+                
+    def out_of_range(x, y) : # 계산한 좌표가 범위 밖으로 나가는지를 계산
+        if (x < 0 or x > WIDTH) or (y < 0 or y > HEIGHT) :
+            return True
+        else :
+            return False
                 
     def update_enemy_positions(ENEMY_LIST, score):
         for idx, ENEMY_POS in enumerate(ENEMY_LIST):  # Using the enumerate function
-            # Updating the position of enemy and making the enemy block fall
-            #print("pos [0] : "+str(ENEMY_POS[0])) x좌표
-            #print("pos [1] : "+str(ENEMY_POS[1])) y좌표
+            # Updating the position of enemy and making the enemy block fall        
             x_move = False
             y_move = False
-            print(ENEMY_POS)
+
+            if not ((ENEMY_POS[0] < 0 or ENEMY_POS[0] > WIDTH) or (ENEMY_POS[1] < 0 or ENEMY_POS[1] > HEIGHT)) : # 범위를 벗어나지 않는 경우에만
+                MATRIX[ENEMY_POS[1]][ENEMY_POS[0]] = False;
+            
             if 0 <= ENEMY_POS[1] <= HEIGHT :
                 ENEMY_POS[1] += ENEMY_POS[3]  # It increments the value of height
-                y_move = True
-                print("세로축 이동")
+                y_move = True            
+
             if 0 <= ENEMY_POS[0] <= WIDTH :
                 ENEMY_POS[0] += ENEMY_POS[2]
-                x_move = True
-                print("가로축 이동")
-            if not x_move or not y_move:
+                x_move = True            
+
+            if not x_move or not y_move: # x 혹은 y 좌표로 움직이지 않은 경우는 끝에 도달했다라는 것임
                 ENEMY_LIST.pop(idx)  # It pops out the enemy from the enemy_list
                 score +=1  # Incrementing the score each time we pass it
+            elif not ((ENEMY_POS[0] < 0 or ENEMY_POS[0] > WIDTH) or (ENEMY_POS[1] < 0 or ENEMY_POS[1] > HEIGHT)) :            
+                MATRIX[ENEMY_POS[1]][ENEMY_POS[0]] = True; # 변경된 좌표에 다시 방문처리    
 
         return score  # It returns the score
 
@@ -199,6 +248,7 @@ while not game_over :  # It keeps running until we hit the game_over condition
 
     Enemy.drop_enemies(ENEMY_LIST)   # Calling the drop enemies function
     score = Enemy.update_enemy_positions(ENEMY_LIST, score)  # It updates the enemy position and stores the score value
+    Enemy.get_input()
     # print(score)  # Prints score to the console
     ENEMY_COUNT = set_level(score, ENEMY_COUNT, ENEMY_LIST)
 
@@ -218,7 +268,6 @@ while not game_over :  # It keeps running until we hit the game_over condition
         # break  # It breaks out of the loop without showing the overlap
 
     Enemy.draw_enemies(ENEMY_LIST)   # Calling the draw enemy function
-
 
     # Drawing the player rectangle
     pygame.draw.rect(screen, RED, (PLAYER_POS[0], PLAYER_POS[1], PLAYER_SIZE, PLAYER_SIZE))  # rect(Surface, Color, Rect, width=0) Look pygame documentatioN
