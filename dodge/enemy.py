64aa9f52252b9ca1767eb061a3ca7f8968b72e92
Minjune <minjune8506@gmail.com>
2022-06-06 15:25:35 +0900

    feat : Enemy 생성 위치 변경
    - 원래 0 좌표에서 생겨났다고 하면 화면 밖에 20px의 여유를 둠

--- dodge/enemy.py
+++ dodge/enemy.py
@@ -8,17 +8,17 @@ class Enemy :
             self.px = random.randint(0, WIDTH)
             self.x_speed = random.uniform(-1, 1)
             if (chooseDir == S) : # 남쪽
-                self.py = HEIGHT
+                self.py = HEIGHT + 20
                 self.y_speed = random.uniform(-1, 0)
             else : # 북쪽
-                self.py = 0
+                self.py = -20
                 self.y_speed = random.uniform(0, 1)
         else : # Case W, E
             self.py = random.randint(0, HEIGHT)
             self.y_speed = random.uniform(-1, 1)
             if (chooseDir == E) : # 동쪽
-                self.px = WIDTH
+                self.px = WIDTH + 20
                 self.x_speed = random.uniform(-1, 0)
             else : # 서쪽
-                self.px = 0
+                self.px = -20
                 self.x_speed = random.uniform(0, 1)
